ff2f4c93392df761928294cae2080896747b0a00
TheoMcavoy <1768681713@qq.com>
2020-01-23 11:37:23 +0800

    blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: '初露锋芒',
+    base:'/blog-demo/',
+    description: '钰翔带你飞',
+    dest: './dist',
+    port: '7777',
+    head: [
+        ['link', {rel: 'icon', href: '/logo.jpg'}]
+    ],
+    markdown: {
+        lineNumbers: true
+    },
+    themeConfig: {
+        nav: require("./nav.js"),
+        sidebar: require('./sidebar.js'),
+        sidebarDepth: 2,
+        lastUpdated: 'Last Updated',
+        searchMaxSuggestoins: 10,
+        serviceWorker: {
+            updatePopup: {
+                message: "有新的内容.",
+                buttonText: '更新'
+            }
+        },
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页 ！'
+    }
+}
---
+++ docs/baodian/high/sidebar.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+        title:'新手指南',
+        collapsable: true,
+        children:[
+            {text:'必读', link:'/baodian/high/notes/one'},
+        ]
+    },
+    {
+        title:'进阶指南',
+        collapsable: true,
+        children:[
+            {text:'必读',link:'/baodian/high/notes/two'},
+        ]
+    }
+]
